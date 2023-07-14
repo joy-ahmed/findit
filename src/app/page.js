6710e3c149886183ced81e61175ebb9c5116d412
Joy Ahmed <dev.joyahmed@gmail.com>
2023-07-14 11:58:23 +0600

    warm up

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import SideNavBar from '@/components/SideNavBar'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="cls">
+      <SideNavBar />
+   </div>
+  )
+}
